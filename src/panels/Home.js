1789392e3ac4fa9e888437d73af8476a89b35d3a
Raimbek <rrakhimbekov@gmail.com>
2019-10-11 00:10:09 +0600

    hooks & https

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Panel, ListItem, Button, Group, Div, Avatar, PanelHeader } from '@vkontakte/vkui';
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import Button from '@vkontakte/vkui/dist/components/Button/Button';
+import Group from '@vkontakte/vkui/dist/components/Group/Group';
+import Div from '@vkontakte/vkui/dist/components/Div/Div';
+
 import persik from "../img/persik.png";
 import logoFull from "../img/logo.png"
 
