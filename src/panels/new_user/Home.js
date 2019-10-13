1b5dbe08902259ff8f0128dbea17d2b57759ce72
IbragimGad <ibrus.rus@gmail.com>
2019-10-13 12:30:20 +0600

    delete unused var

--- src/panels/new_user/Home.js
+++ src/panels/new_user/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
-import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import FixedLayout from "@vkontakte/vkui/dist/components/FixedLayout/FixedLayout";
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 
