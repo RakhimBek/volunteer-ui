a4b6109044ab101d8148f2ac105aeee7a4c452c9
IbragimGad <ibrus.rus@gmail.com>
2019-10-16 17:46:25 +0600

    Tabs activation

--- src/panels/project/Main.js
+++ src/panels/project/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SearchComponent from "../../common/SearchComponent";
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 
@@ -12,7 +12,24 @@ import Icon16Add from '@vkontakte/icons/dist/16/add';
 
 import './Main.css';
 
+const MyTasks = ({go}) =>(
+    <div>
+        <div className="add-task">
+            <Button before={<Icon16Add/>} onClick={go} data-to="new_task">ДОБАВИТЬ</Button>
+        </div>
+        <TaskPreview go={go} image={eg} description="Уважаемые волонтёры! Нам нужно 3 человека в гардероб. Работа с 9:00." startDate="10.11.1993" endDate="11.11.1993" hashtag="#гардероб" arrowVisibility=""/>
+        <TaskPreview go={go} image={eg} description="Задача №2" startDate="11.11.1993" endDate="11.11.1993" hashtag="task12"/>
+        <TaskPreview go={go} image={eg} description="Задача №3" startDate="12.11.1993" endDate="12.11.1993" hashtag="task13"/>
+    </div>
+)
+
+const ArchieveTasks = () =>(
+    <h1>Ты классный, как сыр колбасный ;)</h1>
+)
 const Project = ({id, go}) => {
+    const [tab,setTab] = useState(1);
+
+    const handleTab = (e) => {};
 
     return(
     <Panel id={id} theme="white">
@@ -22,22 +39,20 @@ const Project = ({id, go}) => {
             <List>
                 <Cell>
                     <Tabs type="buttons">
-                        <TabsItem >
+                        <TabsItem data-name={1} onClick={()=>setTab(1)}>
                            Мои задачи
-                        </TabsItem>
-                        <TabsItem >
+                        </TabsItem >
+                        <TabsItem data-name={2} onClick={()=>setTab(2)}>
                             Архив
                         </TabsItem>
                     </Tabs>
                 </Cell>
             </List>
 
-            <div className="add-task">
-            <Button before={<Icon16Add/>} onClick={go} data-to="new_task">ДОБАВИТЬ</Button>
-            </div>
-            <TaskPreview go={go} image={eg} description="Уважаемые волонтёры! Нам нужно 3 человека в гардероб. Работа с 9:00." startDate="10.11.1993" endDate="11.11.1993" hashtag="#гардероб" arrowVisibility=""/>
-            <TaskPreview go={go} image={eg} description="Задача №2" startDate="11.11.1993" endDate="11.11.1993" hashtag="task12"/>
-            <TaskPreview go={go} image={eg} description="Задача №3" startDate="12.11.1993" endDate="12.11.1993" hashtag="task13"/>
+
+            {tab === 1 && <MyTasks/>}
+            {tab === 2 && <ArchieveTasks/>}
+
         </div>
 
         <MenuTabs go={go}/>
