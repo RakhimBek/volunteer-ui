0aa3a8cacba95a298adb9346121d49d72cedc3c3
IbragimGad <ibrus.rus@gmail.com>
2019-10-21 16:56:38 +0600

    layout small fix

--- src/panels/volunteer_profile/volunteer_profile.js
+++ src/panels/volunteer_profile/volunteer_profile.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import MenuTabs from "../../common/MenuTabs";
-import HeaderContext from "@vkontakte/vkui/dist/components/HeaderContext/HeaderContext"
 import './volunteer_profile.css';
 import MenuHeader from "../../common/MenuHeader";
 import UserCard from "../../common/UserCard";
 
-const VolunteerProfile = ({id}) => {
+const VolunteerProfile = ({id, go}) => {
     return(
         <Panel id={id} theme="white">
             <MenuHeader headerTitle="Карточка волонтёра"/>
             <div className="volunteer-profile">
                 <UserCard/>
                 <div>Личная информация которую заполняет сам волонтер</div>
-                <HeaderContext>fsdf</HeaderContext>
             </div>
-            <MenuTabs/>
+            <MenuTabs go={go}/>
         </Panel>
     )
 }
