372046ec8cc21020da59516226945ac0299d6849
Raimbek <rrakhimbekov@gmail.com>
2019-11-06 00:54:53 +0600

    easy swipe

--- src/panels/projects/Project.js
+++ src/panels/projects/Project.js
@@ -1,32 +1,56 @@
-import React from 'react';
+import React, {useState}  from 'react';
 
 import './Project.css'
+import Swipe from "react-easy-swipe";
 
 const Project = ({date, label, go, eventPhoto, role, setActivePanel, SetCurrentProject, currentProject, id}) => {
+    const [testText, setTestText] = useState("inital");
+
+    const onSwipeStart = () => {
+        setTestText("swipeStart");
+    };
+
+    const onSwipeMove = () => {
+        setTestText("onSwipeMove");
+    };
+
+    const onSwipeEnd = () => {
+        setTestText("onSwipeEnd");
+    };
+
     const GoToTasks = (task_id) => {
         SetCurrentProject(task_id);
         window.history.pushState({panel: 'project'}, 'project');
         setActivePanel('project');
-    }
-    return(
+    };
+
+    return (
         <div>
             <div className="event-section event-section-1">
-                <div className="event" style={{backgroundImage: `url(${eventPhoto})`}}>
-                    <div className="event-info">
-                        <p className="event-date">{date}</p>
-                        <p className="event-title">{label}</p>
-                        {role === "organizer" &&
-                        <div className="proj-open-org">
-                            <div className="proj-open-name" onClick={()=>GoToTasks(id)} data-to="project">Открыть</div>
+                <Swipe
+                    onSwipeStart={onSwipeStart}
+                    onSwipeMove={onSwipeMove}
+                    onSwipeEnd={onSwipeEnd}>
+                    <div className="event" style={{backgroundImage: `url(${eventPhoto})`}}>
+                        <div className="event-info">
+                            <p className="event-date">{date}</p>
+                            <p className="event-title">{label}</p>
+                            <p className="event-title">{testText}</p>
+                            {role === "organizer" &&
+                            <div className="proj-open-org">
+                                <div className="proj-open-name" onClick={() => GoToTasks(id)}
+                                     data-to="project">Открыть
+                                </div>
+                            </div>
+                            }
+                            {role === "volunteer" &&
+                            <div className="proj-open-volunteer">
+                                <div className="proj-open-name" onClick={go} data-to="project_description">Открыть</div>
+                            </div>
+                            }
                         </div>
-                        }
-                        {role === "volunteer" &&
-                        <div className="proj-open-volunteer">
-                            <div className="proj-open-name" onClick={go} data-to="project_description">Открыть</div>
-                        </div>
-                        }
                     </div>
-                </div>
+                </Swipe>
             </div>
         </div>
     );
