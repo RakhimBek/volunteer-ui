6ca79ee52ad9abd81ab32e2987aae72bceed75bd
IbragimGad <ibrus.rus@gmail.com>
2019-11-10 01:55:01 +0600

    position sticky

--- src/common/MenuHeader.js
+++ src/common/MenuHeader.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import Icon24Cancel from '@vkontakte/icons/dist/24/cancel';
 import './MenuHeader.css';
-import {FixedLayout} from "@vkontakte/vkui";
-import TabFix from "./TabFix";
 
 
 const MenuHeader = ({headerTitle, closeButton}) => (
-    <div>
-    <FixedLayout className="menu-header-fixed">
         <header className="menu-header">
             <p className="title">{headerTitle}</p>
             {closeButton&&
                 <button className="header-back-button" onClick={() => window.history.back()}><Icon24Cancel/></button>
             }
         </header>
-
-    </FixedLayout>
-    <TabFix height="129px"/>
-    </div>
 );
 
 export default MenuHeader;
--- src/panels/projects/CreateProject.js
+++ src/panels/projects/CreateProject.js
@@ -3,17 +3,12 @@ import React from 'react';
 import './CreateProject.css'
 
 const CreateProject = ({go}) => (
-    <div>
         <div className="new-project">
             <div className="new-project-box">
                 <p className="new-project-title">Новый проект</p>
-                <div className="btn-org">
-                    <div className="btn-name" onClick={go} data-to="new_project">Создать</div>
-                </div>
+                <button className="start-new-project-button" onClick={go} data-to="new_project">Создать</button>
             </div>
         </div>
-    </div>
-
 );
 
 export default CreateProject;
