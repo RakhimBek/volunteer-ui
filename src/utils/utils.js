93fbe71536a434fd4e9d871f0ea93c679758dfc7
Raimbek <rrakhimbekov@gmail.com>
2019-11-09 15:57:44 +0600

    path to host

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,8 @@
 const Utils = {
     path: function (relativePath) {
         console.log('full path to back:  ' + window.location.origin + ':8080/test-api/'.concat(relativePath));
-//        return 'https://raimbek-rakhimbekov.ru:8080/zz/test-api/'.concat(relativePath);
-        return window.location.origin + ':8080/test-api/'.concat(relativePath);
+//        return 'https://raimbek-rakhimbekov.ru:8080/test-api/'.concat(relativePath);
+        return 'https://raimbek-rakhimbekov.ru:8080/test-api/'.concat(relativePath);
     }
 };
 
