eae47660572743213a18548a0bf40f68d0e8400e
Raimbek <rrakhimbekov@gmail.com>
2019-11-11 01:24:24 +0600

    Назад из задач

--- src/panels/new_task/Main.js
+++ src/panels/new_task/Main.js
@@ -12,7 +12,7 @@ import axios from 'axios/dist/axios';
 
 import './Main.css';
 
-const NewTask = ({id, go, projectId}) => {
+const NewTask = ({id, go, projectId, setState}) => {
     const [taskName, setTaskName] = useState("");
 
     const handleTaskName = (e) => {
@@ -43,6 +43,7 @@ const NewTask = ({id, go, projectId}) => {
                 console.log('Task.BAD');
             });
 
+        go(e);
         e.preventDefault();
     };
 
@@ -56,7 +57,7 @@ const NewTask = ({id, go, projectId}) => {
                     <Input className="date-input" top="Дата окончания" type="date"/>
                 </div>
                 <Textarea top="Описание мироприятия" placeholder="Группы,исполнители,композиторы"/>
-                <Button size="xl" onClick={save}>Создать</Button>
+                <Button size="xl" onClick={save} data-to="project">Создать</Button>
             </FormLayout>
             <TabFix height="80px"/>
             <MenuTabs go={go}/>
