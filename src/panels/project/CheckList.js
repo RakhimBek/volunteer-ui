4e4aa08af7b8c89128f2bc2c8bd79fb992239583
Raimbek <rrakhimbekov@gmail.com>
2019-11-14 00:07:37 +0600

    refactor

---
+++ src/panels/project/CheckList.js
@@ -0,0 +1,127 @@
+import React, {useState, useEffect} from 'react';
+import Utils from "../../utils/utils";
+import {Checkbox} from "@vkontakte/vkui";
+import axios from 'axios/dist/axios'
+import Icon24Add from '@vkontakte/icons/dist/24/add';
+
+const CheckList = ({projectId}) => {
+    const [noteText, setNoteText] = useState();
+    const [noteType, setNoteType] = useState(1);
+    const [noteStates, setNoteStates] = useState([]);
+    const [noteList, setNoteList] = useState([]);
+    const [categories, setCategories] = useState([]);
+
+    const addNote = (e) => {
+        console.log('addNote');
+        axios
+            .post(Utils.path('project/' + projectId + '/note'), {
+                "text": noteText,
+                "category": noteType,
+                "completed": false,
+            })
+            .then((response) => {
+                console.log('add note. good: ' + JSON.stringify(response.data));
+                setNoteList([response.data, ...noteList]);
+            })
+            .catch((reason) => {
+                console.log('add note.  bad: ' + reason)
+            });
+
+        e.preventDefault();
+    };
+
+    useEffect(() => {
+        axios
+            .get(Utils.path('project/' + projectId + '/note'))
+            .then((response) => {
+                setNoteStates(response.data.reduce((acc, el) => acc.concat([el.completed]), []));
+                setNoteList(response.data);
+            });
+
+    }, [projectId]);
+
+    const handleCheck = (e) => {
+        console.log(e.currentTarget.dataset.index);
+        const index = e.currentTarget.dataset.index;
+        const stateCopy = [...noteStates];
+        stateCopy[index] = !stateCopy[index];
+        setNoteStates(stateCopy);
+
+        const info = e.currentTarget.dataset.info;
+        const note_edit = {
+            "completed": stateCopy[index]
+        };
+
+        /*
+            почти все ок но тут добавляется новая запись. Надо бэк поправить.
+            Долго мучился. Ничего больше не сделал :(
+        */
+        axios
+            .post(Utils.path('project/' + projectId + '/note/' + info.id), note_edit)
+            .then((response) => {
+                console.log('handle check.good: ' + response);
+            })
+            .catch((reason) => {
+                console.log('handle check.bad: ' + reason);
+            });
+
+        e.preventDefault();
+    };
+
+    const Notes = ({noteDescriptions}) => {
+        return noteDescriptions.map((el, index) => {
+            return <Note noteDescription={el} index={index} state={noteStates[index]}/>
+        });
+    };
+
+    const Note = ({noteDescription, index, state}) => {
+        console.log('Note: ' + state);
+
+        return (
+            <Checkbox onChange={handleCheck}
+                      data-info={noteDescription}
+                      data-index={index}
+                      checked={state}>{noteDescription.text}</Checkbox>
+        );
+    };
+
+    useEffect(() => {
+        axios
+            .get(Utils.path('note/category'))
+            .then((response) => {
+                setCategories(response.data.reduce((acc, el) => acc.concat(el), []));
+                console.log('note/category.good: ' + response);
+            })
+            .catch((reason) => {
+                console.log('note/category.bad: ' + reason);
+            });
+    }, []);
+
+    const Categories = ({categoryDescriptions}) => categoryDescriptions.map((el) => (
+        <option value={el.id}>{el.name}</option>)
+    );
+
+    return (
+        <div>
+            <form className="check-list-form">
+                <input type="text"
+                       name="check-list-input"
+                       placeholder="Введите новую задачу сюда"
+                       onChange={e => setNoteText(e.target.value)}
+                       className="check-list-input"/>
+
+                <select className="note-category-selection" onChange={e => setNoteType(e.target.value)}>
+                    <Categories categoryDescriptions={categories}/>
+                </select>
+                <button className="add-task-to-list-btn" onClick={addNote}>
+                    <Icon24Add/>
+                </button>
+            </form>
+            <div className="check-list-items">
+                <Notes noteDescriptions={noteList}/>
+            </div>
+        </div>
+    );
+};
+
+export default CheckList;
--- src/panels/project/Main.js
+++ src/panels/project/Main.js
@@ -2,14 +2,10 @@ import React, {useState, useEffect} from 'react';
 import SearchComponent from "../../common/SearchComponent";
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 
-/*import TaskPreview from "./TaskPreview";
-import eg from '../../img/play_24.png'*/
-
 import MenuTabs from "../../common/MenuTabs";
 import MenuHeader from "../../common/MenuHeader";
-import {Button, Tabs, TabsItem, Cell, List, Checkbox} from "@vkontakte/vkui";
+import {Button, Tabs, TabsItem, Cell, List} from "@vkontakte/vkui";
 import Icon16Add from '@vkontakte/icons/dist/16/add';
-import Icon24Add from '@vkontakte/icons/dist/24/add';
 
 import axios from 'axios/dist/axios'
 import Utils from "../../utils/utils"
@@ -17,127 +13,8 @@ import Utils from "../../utils/utils"
 import './Main.css';
 import TabFix from "../../common/TabFix";
 import TaskPreview from "./TaskPreview";
+import CheckList from "./CheckList";
 import eg from "../../img/play_24.png";
-
-const CheckList = ({projectId}) => {
-    const [noteText, setNoteText] = useState();
-    const [noteType, setNoteType] = useState(1);
-    const [noteStates, setNoteStates] = useState([]);
-    const [noteList, setNoteList] = useState([]);
-    const [categories, setCategories] = useState([]);
-
-    const addNote = (e) => {
-        console.log('addNote');
-        axios
-            .post(Utils.path('project/' + projectId + '/note'), {
-                "text": noteText,
-                "category": noteType,
-                "completed": false,
-            })
-            .then((response) => {
-                console.log('add note. good: ' + JSON.stringify(response.data));
-                setNoteList([response.data, ...noteList]);
-            })
-            .catch((reason) => {
-                console.log('add note.  bad: ' + reason)
-            });
-
-        e.preventDefault();
-    };
-
-    useEffect(() => {
-        axios
-            .get(Utils.path('project/' + projectId + '/note'))
-            .then((response) => {
-                setNoteStates(response.data.reduce((acc, el) => acc.concat([el.completed]), []));
-                setNoteList(response.data);
-            });
-
-    }, [projectId]);
-
-    const handleCheck = (e) => {
-        console.log(e.currentTarget.dataset.index);
-        const index = e.currentTarget.dataset.index;
-        const stateCopy = [...noteStates];
-        stateCopy[index] = !stateCopy[index];
-        setNoteStates(stateCopy);
-
-        const info = e.currentTarget.dataset.info;
-        const note_edit = {
-            "completed": stateCopy[index]
-        };
-
-        /*
-            почти все ок но тут добавляется новая запись. Надо бэк поправить.
-            Долго мучился. Ничего больше не сделал :(
-        */
-        axios
-            .post(Utils.path('project/' + projectId + '/note/' + info.id), note_edit)
-            .then((response) => {
-                console.log('handle check.good: ' + response);
-            })
-            .catch((reason) => {
-                console.log('handle check.bad: ' + reason);
-            });
-
-        e.preventDefault();
-    };
-
-    const Notes = ({noteDescriptions}) => {
-        return noteDescriptions.map((el, index) => {
-            return <Note noteDescription={el} index={index} state={noteStates[index]}/>
-        });
-    };
-
-    const Note = ({noteDescription, index, state}) => {
-        console.log('Note: ' + state);
-
-        return (
-            <Checkbox onChange={handleCheck}
-                      data-info={noteDescription}
-                      data-index={index}
-                      checked={state}>{noteDescription.text}</Checkbox>
-        );
-    };
-
-    useEffect(() => {
-        axios
-            .get(Utils.path('note/category'))
-            .then((response) => {
-                setCategories(response.data.reduce((acc, el) => acc.concat(el), []));
-                console.log('note/category.good: ' + response);
-            })
-            .catch((reason) => {
-                console.log('note/category.bad: ' + reason);
-            });
-    }, []);
-
-    const Categories = ({categoryDescriptions}) => categoryDescriptions.map((el) => (
-        <option value={el.id}>{el.name}</option>)
-    );
-
-    return (
-        <div>
-            <form className="check-list-form">
-                <input type="text"
-                       name="check-list-input"
-                       placeholder="Введите новую задачу сюда"
-                       onChange={e => setNoteText(e.target.value)}
-                       className="check-list-input"/>
-
-                <select className="note-category-selection" onChange={e => setNoteType(e.target.value)}>
-                    <Categories categoryDescriptions={categories}/>
-                </select>
-                <button className="add-task-to-list-btn" onClick={addNote}>
-                    <Icon24Add/>
-                </button>
-            </form>
-            <div className="check-list-items">
-                <Notes noteDescriptions={noteList}/>
-            </div>
-        </div>
-    );
-};
 
 const Project = ({id, go, role, activePanel, projectId, setState}) => {
     const [tab, setTab] = useState(1);
