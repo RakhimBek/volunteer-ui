65b215d44d41951c849287cb21a09cfc87d91253
Raimbek <rrakhimbekov@gmail.com>
2019-11-16 17:41:58 +0600

    refactor

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,19 +3,20 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'ORGANIZER_PROJECTS') {
+    switch (action.type) {
+        case 'ORGANIZER_PROJECTS':
 
-        return Object.assign({}, state, {
-            organizerProjects: action.organizerProjects
-        });
+            return Object.assign({}, state, {
+                organizerProjects: action.organizerProjects
+            });
 
-    } else if (action.type === 'DELETE_PROJECT') {
-        const organizerProjects = new Map(state.organizerProjects);
-        organizerProjects.delete(action.projectId);
+        case 'DELETE_PROJECT':
+            const organizerProjects = new Map(state.organizerProjects);
+            organizerProjects.delete(action.projectId);
 
-        return Object.assign({}, state, {
-            organizerProjects: organizerProjects
-        });
+            return Object.assign({}, state, {
+                organizerProjects: organizerProjects
+            });
     }
 
     return state;
