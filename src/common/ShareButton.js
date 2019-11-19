a9b98c1e8555d9279f3aad58549ed693b8d2fbd9
IbragimGad <ibrus.rus@gmail.com>
2019-11-19 16:41:14 +0600

    share butt

--- src/common/ShareButton.js
+++ src/common/ShareButton.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import connect from '@vkontakte/vk-connect';
 import "./ShareButton.css";
 
-const ShareButton = () => (
-    <button className="share-project-button">Поделиться</button>
-);
-
+const ShareButton = () => {
+    const share = () => {
+        connect.send("VKWebAppShowWallPostBox", {
+            message: "Я участвую в волонтёрском проекте в приложении Дай пять",
+            attachments:"https://vk.com/app7150209"});
+}
+    return (
+        <button onClick={share} className="share-project-button">Поделиться</button>
+    );
+}
 export default ShareButton;
