6048354aa518c928e6cd27bcd3830c4f551ab075
Raimbek <rrakhimbekov@gmail.com>
2019-12-15 12:54:23 +0600

    back url

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 const Utils = {
     path: function (relativePath) {
-        console.log('window.location.origin:  ' + window.location.origin + ' full path to back:  ' + 'https://raimbek-rakhimbekov.ru:8080/test-api/'.concat(relativePath));
-        return 'https://raimbek-rakhimbekov.ru:8080/test-api/'.concat(relativePath);
+        console.log('window.location.origin:  ' + window.location.origin + ' full path to back:  ' + relativePath);
+        return 'https://api-chatty.herokuapp.com/ggg/'.concat(relativePath);
     }
 };
 
