185b3c0108056567af7564dbc5562fcd97c2c74f
Raimbek <rrakhimbekov@gmail.com>
2019-12-15 19:36:11 +0600

    back url

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 npm run build
-scp -i "id_rsa.pem" -r build "ubuntu@ec2-18-189-178-202.us-east-2.compute.amazonaws.com:"
-ssh -i "id_rsa.pem" ubuntu@ec2-18-189-178-202.us-east-2.compute.amazonaws.com "sudo rm -rf /var/www/html/volunteer/*"
-ssh -i "id_rsa.pem" ubuntu@ec2-18-189-178-202.us-east-2.compute.amazonaws.com "sudo mv build/* /var/www/html/volunteer"
+scp -i "id_rsa.pem" -r build "ubuntu@ec2-52-15-183-181.us-east-2.compute.amazonaws.com:"
+ssh -i "id_rsa.pem" ubuntu@ec2-52-15-183-181.us-east-2.compute.amazonaws.com "sudo rm -rf /var/www/html/volunteer/*"
+ssh -i "id_rsa.pem" ubuntu@ec2-52-15-183-181.us-east-2.compute.amazonaws.com "sudo mv build/* /var/www/html/volunteer"
 echo 'Done!'
