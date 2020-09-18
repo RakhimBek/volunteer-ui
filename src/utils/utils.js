5620dea036f76faf7562b9de312e2a725fbf9786
Raimbek <rrakhimbekov@gmail.com>
2020-09-18 19:55:28 +0600

    api link

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 const Utils = {
     path: function (relativePath) {
         console.log('window.location.origin:  ' + window.location.origin + ' full path to back:  ' + relativePath);
-        return 'https://raimbek-rakhimbekov.ru:8080/ggg/'.concat(relativePath);
+        return 'https://volunteerapi.herokuapp.com/api/'.concat(relativePath);
     }
 };
 
